4c63cdb691ef061f8404f61d1ba7070fe8605b00
Hoang Nguyen <88585967+hnthedev@users.noreply.github.com>
2026-04-23 23:36:00 +0700

    Add files via upload

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,23 @@
+﻿function prompt {
+    $lastCommandSuccess = $?
+
+    $currentFolder = Split-Path $pwd -Leaf
+    if ($currentFolder -eq "") { $currentFolder = $pwd.Path }
+    if ($pwd.Path -eq $HOME) { $currentFolder = "~" }
+
+    # Using [char]27 ensures compatibility with older PowerShell 5.1
+    $esc = [char]27
+    $hnColor     = "$esc[38;2;227;100;100m"
+    $folderColor = "$esc[38;2;86;182;194m"
+    $reset       = "$esc[0m"
+
+    if (-not $lastCommandSuccess) {
+        $arrowColor = "$esc[38;2;255;0;0m" 
+    } else {
+        $arrowColor = "$esc[38;2;98;237;139m"
+    }
+
+    Write-Host "${hnColor}@hn ${arrowColor}➜ ${folderColor}$currentFolder${reset}" -NoNewline
+    
+    return " "
+}
